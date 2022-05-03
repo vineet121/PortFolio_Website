a8f7865bf1a9362fb42771b1d8329f8da6c9e0d4
vineet121 <vineetdahiya2000@gmail.com>
2022-05-04 04:27:16 +0530

    FIXED SECURITY

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -2,7 +2,6 @@ import { useEffect, useRef } from 'react'
 import gsap from 'gsap-trial'
 import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoS from '../../../assets/images/logo-p.png'
-import PersonalPhoto from "../../../assets/images/Vineet.png"
 import './index.scss'
 
 const Logo = () => {
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,11 +2,9 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import LogoTitle from '../../assets/images/logo-s.png'
 import Logov from '../../assets/images/logo-v.png'
 import Logo from './Logo'
 import './index.scss'
-
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
